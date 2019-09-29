44c589c652286a40a086ab434fd77542a97511f7
Cyrus <wiranenar@gmail.com>
2019-09-29 23:52:07 +0300

    refactoring 'task 13'

--- smile.py
+++ smile.py
@@ -8,7 +8,7 @@ turtle.shape('turtle')
 
 def draw_shape(shape, radius, dir):
     i = 0
-    angle = 1
+    angle = 10
     length = 2 * math.pi * radius
     step = (length / 360) * angle
     amount = 180 if shape == 'arc' else 360
@@ -17,50 +17,50 @@ def draw_shape(shape, radius, dir):
         turtle.right(angle) if dir == 0 else turtle.left(angle)
         i += angle
 
-def smile(face_radius, eye_radius, nouse_length, smile_radius):
+def smile(radius):
     turtle.begin_fill()
-    draw_shape('circle', face_radius, 1)
+    draw_shape('circle', radius, 1)
     turtle.color('yellow')
     turtle.end_fill()
     turtle.color('black')
-    
+
     turtle.penup()
-    turtle.goto(-(face_radius * 0.25), (face_radius * 2) * 0.75)
+    turtle.goto(-(radius * 0.25), (radius * 2) * 0.75)
     turtle.pendown()
 
     turtle.begin_fill()
-    draw_shape('circle', eye_radius, 0)
+    draw_shape('circle', radius * 0.15, 0)
     turtle.color('blue')
     turtle.end_fill()
     turtle.color('black')
 
     turtle.penup()
-    turtle.goto((face_radius * 0.25) + eye_radius, turtle.ycor())
+    turtle.goto((radius * 0.25) + radius * 0.15, turtle.ycor())
     turtle.pendown()
 
     turtle.begin_fill()
-    draw_shape('circle', eye_radius, 0)
+    draw_shape('circle', radius * 0.15, 0)
     turtle.color('blue')
     turtle.end_fill()
     turtle.color('black')
 
     turtle.penup()
-    turtle.goto(turtle.xcor() - eye_radius * 2, turtle.ycor() - (eye_radius) - face_radius * 0.25)
+    turtle.goto(turtle.xcor() - (radius * 0.3), turtle.ycor() - radius * 0.5)
     turtle.pendown()
 
     turtle.right(90)
-    turtle.width(10)
-    turtle.forward(nouse_length)
+    turtle.width(4)
+    turtle.forward(radius * 0.25)
 
     turtle.penup()
-    turtle.goto(turtle.xcor() + eye_radius * 2, turtle.ycor() - face_radius * 0.25)
+    turtle.goto(turtle.xcor() + (radius * 0.25), turtle.ycor() - radius * 0.25)
     turtle.pendown()
 
-    turtle.width(8)
+    turtle.width(4)
     turtle.color('red')
-    draw_shape('arc', smile_radius, 0)
+    draw_shape('arc', radius * 0.25, 0)
     turtle.color('black')
 
-smile(100, 15, 30, 30)
+smile(200)
 
 screen.mainloop()
